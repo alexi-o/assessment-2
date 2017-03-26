0e23fc94bff167de5f6e3aeee85789552a951521
AlexiOHearn <alexiohearn@o.local>
2017-03-26 12:16:59 -0600

    adds checkForWin function

--- js/app.js
+++ js/app.js
@@ -1,49 +1,56 @@
+// This function checks for a winner by running after each keystroke and checking relative position.
+var checkForWin = function(){ 
+	console.log("Checking for win!");
+	if($('.orangeCar').css('left') === '400') {
+		alert("Blue wins!");
+	} else if ($('.greenCar').css('left') === '400') {
+			alert("Green wins!");
+		} else {
+
+		}
+}
+
+var car1X = $('.orangeCar').offset('left');
+var car2X = $('.greenCar').offset('left');
+
 console.log("yay!");
 		$(document).keydown(function(e) {
 	    switch (e.which) {
-	    case 37:
-	        $('.greenPlayer').stop().animate({
-	            left: '-=40'
-	        }); //left arrow key
-	        break;
 	    case 38:
-	        $('.greenPlayer').stop().animate({
+	        $('.greenCar').stop().animate({
 	            top: '-=40'
-	        }); //up arrow key
+	        }); 
 	        break;
 	    case 39:
-	        $('.greenPlayer').stop().animate({
+	        $('.greenCar').stop().animate({
 	            left: '+=40'
-	        }); //right arrow key
+	        });
+	        	checkForWin(); 
 	        break;
 	    case 40:
-	        $('.greenPlayer').stop().animate({
+	        $('.greenCar').stop().animate({
 	            top: '+=40'
-	        }); //bottom arrow key
+	        }); 
 	        break;
 	    }
 	});
 		$(document).keydown(function(e) {
 	    switch (e.which) {
-	    case 65:
-	        $('.bluePlayer ').stop().animate({
-	            left: '-=40'
-	        }); //left arrow key
-	        break;
 	    case 87:
-	        $('.bluePlayer ').stop().animate({
+	        $('.orangeCar ').stop().animate({
 	            top: '-=40'
-	        }); //up arrow key
+	        }); 
 	        break;
 	    case 68:
-	        $('.bluePlayer ').stop().animate({
+	        $('.orangeCar ').stop().animate({
 	            left: '+=40'
-	        }); //right arrow key
+	        });
+	        	checkForWin(); 
 	        break;
 	    case 83:
-	        $('.bluePlayer ').stop().animate({
+	        $('.orangeCar ').stop().animate({
 	            top: '+=40'
-	        }); //bottom arrow key
+	        }); 
 	        break;
 	    }
 	});
